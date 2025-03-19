59fed73b82c0c2c27aec84396c94f23bc96f2760
AlexKaplan <alexkaplan110606@gmail.com>
2025-03-19 16:17:19 +0200

    feat(main): add header

--- src/main.js
+++ src/main.js
@@ -1,18 +1,3 @@
-// (() => {
-//   const refs = {
-//     // Додати атрибут data-modal-open на кнопку відкриття
-//     openModalBtn: document.querySelector("[data-modal-open]"),
-//     // Додати атрибут data-modal-close на кнопку закриття
-//     closeModalBtn: document.querySelector("[data-modal-close]"),
-//     // Додати атрибут data-modal на бекдроп модалки
-//     modal: document.querySelector("[data-modal]"),
-//   };
+'use strict';
 
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
-
-//   function toggleModal() {
-//     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-//     refs.modal.classList.toggle("is-open");
-//   }
-// })();
+import './js/main/header.js';
